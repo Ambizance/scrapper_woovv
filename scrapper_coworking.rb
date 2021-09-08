4307bf8b43451c9fb11ef92d5184f486c5d86f61
jonathan justman <jojo.justman@gmail.com>
2021-09-08 11:29:13 +0200

    avant de tout casser

--- scrapper_coworking.rb
+++ scrapper_coworking.rb
@@ -25,7 +25,7 @@ end
 def get_all_coworking_url(page_index_general)
     tab_all_url = []
     CSV.open("db_urls.csv", "wb") do |csv|
-        csv << ["CW_url", "CW_name", "address", "zipcode", "city", "pricing", "opening_hours", "facilities"]
+        csv << ["CW_name", "CW_description", "CW_url", "address", "pricing", "opening_hours", "website_url"] # ne pas oublier de remettre "city" et "zipcode"
         for i in 1..2
             page = page_index_general + i.to_s
             get_coworking_url(page).each do |url|
@@ -43,25 +43,21 @@ def get_cw_infos_from_url(url_coworking)
     page = Nokogiri::HTML(URI.open(url_coworking))   
     # return page.css('h1.product_title').text
     cw_info_tab = []
-    cw_info_tab << page.css('h1.product_title').text
-    cw_info_tab << url_coworking
-    # cw_info_tab << page.css('.rh-360-content-area tabletsblockdisplay p')[1].text
-
-    puts "😁"*20
-    puts page.css('.rh-360-content-area p')[2].text
-    puts "😁"*20
-
-
-    # //*[@id="section-description"]/div/div/p[2]
-
-
-    # return cw_info_tab
+    cw_info_tab << page.css('h1.product_title').text # name
+    cw_info_tab << page.css('.rh-360-content-area p')[0].text  # description
+    cw_info_tab << url_coworking # URL
+    cw_info_tab << page.css('.rh-360-content-area p')[2].text  # adresse complète
+    cw_info_tab << page.css('.rh-360-content-area p')[11].text  # tarifs
+    cw_info_tab << page.css('.rh-360-content-area p')[9].text  # horaires
+    # cw_info_tab << page.css('.rh-360-content-area p')[17].text  # facilities >>> BIZARRE CETTE HISTOIRE
+    cw_info_tab << page.css('.rh-360-content-area p a')[0]['href'] # penser à retirer le lien affilié coworking-france par la suite
+    return cw_info_tab
 end
 
-puts '*'*100
+# puts '*'*100
 # print get_cw_infos_from_url('https://www.coworking-france.com/espace-coworking/coworking-digital-village-seine-et-marne/')
-puts get_cw_infos_from_url('https://www.coworking-france.com/espace-coworking/coworking-digital-village-seine-et-marne/')
-puts '*'*100
+print get_cw_infos_from_url('https://www.coworking-france.com/espace-coworking/coworking-digital-village-seine-et-marne/').count
+# puts '*'*100
 
 
 
