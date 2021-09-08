f5298ec85d2dbb69248c043fc849ad9de343bcc6
jonathan justman <jojo.justman@gmail.com>
2021-09-08 10:06:47 +0200

    récupération de la la liste complète des URLs de coworking OK

--- scrapper_coworking.rb
+++ scrapper_coworking.rb
@@ -1,36 +1,53 @@
 require 'rubygems'
 require 'nokogiri' 
 require 'open-uri'
+require 'csv'
 
 
-starting_url = Nokogiri::HTML(URI.open("https://www.coworking-france.com/coworking/coworking-france/coworking-bretagne/coworking-ille-et-vilaine/"))   
+# Fonction qui récupère la liste d'URL d'une page d'index donnée 
 
-# puts link_coworking
-
-def get_coworking_url(page_list)
-    page = Nokogiri::HTML(URI.open(page_list))   
+def get_coworking_url(page_liste_24_results)
+    page = Nokogiri::HTML(URI.open(page_liste_24_results))   
     link_coworking = page.css('h3 a')
     url_tab = []
     link_coworking.each do |lien|
         # url_tab << lien['href']
         url_tab << lien['href']
     end
+    return url_tab
+    
+end
 
 
-     return url_tab
+# Fonction qui parcourt chaque page de l'index (105 pages et applique la fonction 
+# get_coworking_url sur chacune d'entre elle et stocke toutes les URLs dans un tableau
+
+def get_all_coworking_url(page_index_general)
+
+    tab_all_url = []
+    for i in 1..105
+        page = page_index_general + i.to_s
+        get_coworking_url(page).each do |url|
+            tab_all_url << url
+        end
+    end
+    return tab_all_url
 
 end
+
+get_all_coworking_url('https://www.coworking-france.com/coworking/coworking-france/page/').count
+
+
 
 def get_title_from_url(url_coworking)
     page = Nokogiri::HTML(URI.open(url_coworking))   
     return page.css('h1.product_title').text
-
 end
 
-# puts get_coworking_url('https://www.coworking-france.com/coworking/coworking-france/coworking-bretagne/coworking-ille-et-vilaine/')
+# puts get_coworking_url('https://www.coworking-france.com/coworking/coworking-france/')
 
 # puts get_title_from_url('https://www.coworking-france.com/espace-coworking/coworking-la-cantine-numerique-rennes/')
 # return title = page.css('h1.product_title').text.split[1..-1].join(' ')
 
 
-puts get_title_from_url(get_coworking_url('https://www.coworking-france.com/coworking/coworking-france/coworking-bretagne/coworking-ille-et-vilaine/')[23])
+# puts get_title_from_url(get_coworking_url('https://www.coworking-france.com/coworking/coworking-france/coworking-bretagne/coworking-ille-et-vilaine/')[23])
